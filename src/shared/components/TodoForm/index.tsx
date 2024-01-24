91133eb15a99e0a4f58f1a2713103a3c8fba6e9c
Bohdan <bogdaneskor@gmail.com>
2024-01-24 22:02:05 +0200

    style fix

--- src/shared/components/TodoForm/index.tsx
+++ src/shared/components/TodoForm/index.tsx
@@ -70,12 +70,12 @@ const TodoForm: FC<TodoFormProps> = ({ onSubmit }) => {
                                     placeholder="Add Todo..."
                                     _focus={{
                                         border: "2px solid",
-                                        borderColor: formState.errors.todo ? 'red.500' : 'blue.500',
+                                        borderColor: formState.errors.title ? 'red.500' : 'blue.500',
                                         boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'
                                     }}
                                     _active={{
                                         border: "2px solid",
-                                        borderColor: formState.errors.todo? 'red.500' : 'blue.500',
+                                        borderColor: formState.errors.title? 'red.500' : 'blue.500',
                                         boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'
                                     }}
                                     {...rest}
