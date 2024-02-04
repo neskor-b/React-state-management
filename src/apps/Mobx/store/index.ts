535e5c3dc1a7949af7cc35a08011b0178cfb682f
Bohdan <bogdaneskor@gmail.com>
2024-02-04 12:00:24 +0200

    changed root store

--- src/apps/Mobx/store/index.ts
+++ src/apps/Mobx/store/index.ts
@@ -1,12 +1,7 @@
-import { makeAutoObservable } from 'mobx';
-
 import TodosStore from './todos';
 
 class RootStore {
     todos = new TodosStore();
-    constructor() {
-        makeAutoObservable(this)
-    }
 }
 export type { RootStore } ;
 
