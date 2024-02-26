6ccb7f82c430ef9394bdd5edc9835fd2698dbf48
Bohdan <bogdaneskor@gmail.com>
2024-02-26 18:47:44 +0200

    Separate styles for settings

--- src/shared/components/Settings/styled.ts
+++ src/shared/components/Settings/styled.ts
@@ -6,9 +6,9 @@ export const Container = styled<{ isDesktop: boolean }>(Flex, ({ isDesktop }) =>
     {
         position: "absolute",
         width: "fit-content",
+        height: "fit-content",
         gap: 2,
         zIndex: 100,
-        height: "fit-content",
         right: isDesktop ? 5 : undefined,
         left: isDesktop ? undefined : 2,
         bottom: isDesktop ? 5 : undefined,
