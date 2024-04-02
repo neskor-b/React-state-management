ff720e243aba73d4f6894863ef411b347e45bbc9
Bohdan <bogdaneskor@gmail.com>
2024-04-02 22:02:01 +0300

    fix i18Next

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,21 +1,19 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 import engTranslation from 'locale/en/translation.json';
 import uaTranslation from 'locale/ua/translation.json';
 
 i18next
     .use(initReactI18next)
-    .use(LanguageDetector)
     .init({
         fallbackLng: 'en',
         debug: false,
         resources: {
-            en: {
+            'en': {
                 translation: engTranslation
             },
-            ua: {
+            'ua': {
                 translation: uaTranslation
             }
         }
